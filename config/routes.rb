175a52299d59d272ee9ac64424fb25a50de551bb
andreea grigorescu <deea26@gmail.com>
2016-11-19 15:40:49 +0000

    added an about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/info'
-
+  get 'pages/about'
   root to: redirect('/ideas')
+  
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
